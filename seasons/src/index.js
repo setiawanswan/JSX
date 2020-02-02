c4b4a2ccafd6bf6d54effd010802edf3a83642ad
setiawanswan <setiawan.tumilin@gmail.com>
2020-02-02 15:55:24 +0700

    state in react

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    constructor(props){
+        super(props);
+
+        this.state = {lat: null, errorMessage: ''}
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        ); 
+            position => {
+                // We called set state 
+                this.setState({lat: position.coords.latitude})
+            },
+            err => {
+                this.setState({errorMessage: err.message})
+            }
+        );  
+    }
+
+// React says we have to define render !!
+    render() {
         return (
             <div>
-                Latitude :
+                Latitude : {this.state.lat}
+                <br/>
+                Error : {this.state.errorMessage}
             </div>
         )
     }
