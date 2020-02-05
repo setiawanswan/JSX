ddb77ff7df4edeee674c98db494c8bf210d4955b
setiawanswan <setiawan.tumilin@gmail.com>
2020-02-05 16:37:05 +0700

    alternate state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,31 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
+    state = {lat: null, errorMessage: ''}
 
-        this.state = {lat: null, errorMessage: ''}
-
+    componentDidMount (){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // We called set state 
-                this.setState({lat: position.coords.latitude})
-            },
-            err => {
-                this.setState({errorMessage: err.message})
-            }
-        );  
+            position =>this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})    
+        );
     }
 
 // React says we have to define render !!
     render() {
-        return (
-            <div>
-                Latitude : {this.state.lat}
-                <br/>
-                Error : {this.state.errorMessage}
-            </div>
-        )
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error : {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat){
+        return <div>Latitude : {this.state.lat}</div>;
+        }
+
+        return <div>Loading !</div>;
     }
 }
 
