e2ef7e6cb73c13ae2128cab58ae7a98fb8860f40
setiawanswan <setiawan.tumilin@gmail.com>
2020-02-06 15:13:29 +0700

    state in react use this

--- statenprops/src/profile.js
+++ statenprops/src/profile.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-const Profile = (props) =>{
-    const style = {
-        padding: '10px',
-        border: '1px solid green',
-        display: 'block',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        width: '50%',
-        color: '#4db1e8',
-        textAlign: 'center',
-        fontFamily: 'sans-serif'
-    }
+class Profile extends React.Component {
+    render(){
+        const style = {
+            padding: '10px',
+            border: '1px solid green',
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '50%',
+            color: '#4db1e8',
+            textAlign: 'center',
+            fontFamily: 'sans-serif'
+        }
 
-    return (
-        <div style={style}>
-            <img src={props.logo_url} height="250px" alt="" />
-            <h1>{props.title}</h1>
-        </div>
-    )
+        return(
+            <div style={style}>
+                <img src={this.props.logo_url} alt="" height="240px"/>
+                <h1>{this.props.title}</h1>
+            </div>
+        );
+    }
 }
 
 export default Profile;
