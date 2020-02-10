61349da29243a6bab18a160f3d7ba9cac5e28cc8
setiawanswan <setiawan.tumilin@gmail.com>
2020-02-10 14:05:21 +0700

    add css in react

--- seasons/src/SeasonsDisplay.js
+++ seasons/src/SeasonsDisplay.js
@@ -1,11 +1,38 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonsDisplay = () => {
-    return(
-        <div>
-            Season Display
-        </div>
-    )
+const seasonConfig = {
+    summer: {
+        text : 'Let\'s hit the beach' ,
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it is cold',
+        iconName: 'snowflake'       
+    }
+};
+
+const getSeasons = (lat, month) => {
+    if (month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter'
+    } else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SeasonsDisplay = props => {
+    // console.log(props.lat);
+    const seasons = getSeasons(props.lat, new Date().getMonth())
+    const {text, iconName} = seasonConfig[seasons];
+    // console.log(seasons) 
+
+return(
+    <div className={`season-display ${seasons}`}>
+        <i className={`icon-left massive ${iconName} icon`} />
+        <h1>{text}</h1>
+        <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+) 
 }
 
 export default SeasonsDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonsDisplay from './SeasonsDisplay';
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''}
@@ -18,7 +19,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat){
-        return <div>Latitude : {this.state.lat}</div>;
+        return <SeasonsDisplay lat={this.state.lat} />
         }
 
         return <div>Loading !</div>;
