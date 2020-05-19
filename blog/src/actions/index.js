efdcb990f3cf465b3fe7595c2e434cfd3d7022bd
setiawanswan <setiawan.tumilin@gmail.com>
2020-05-19 14:55:11 +0700

    action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,39 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+    console.log('About to fetch posts')
+    await dispatch(fetchPosts());
+    console.log('Fetched Posts !')
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-
+ 
     dispatch({ type: 'FETCH_POST', payload: response.data })
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data})
+};
+
+// export const fetchUser = (id) => async dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data})
+// });
+
+
+// export const fetchUser = function(id){ 
+
+//     return _.memoize(async function (dispatch){
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//         dispatch({ type: 'FETCH_USER', payload: response.data});
+//     });
+// }; 
